20a49fad6a6e3f033f832001e2b524053a289898
shashank <shekhar824947@gmail.com>
2023-01-19 01:26:30 +0530

    used new api

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express=require("express");
-const app=express();
+const axios=require("axios")
+const moment = require('moment')
 
+const app=express();
+app.locals.moment = moment;
 const port=process.env.PORT||3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use("/",require("./routes/route"))
+app.set('views','./views')
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,26 @@
-const express=require("express");
-const router=express.Router();
-// const Blog=require("../models/todo")
 
-router.get("/",(req,res)=>{
-   
-    res.render("index")
+const express = require('express')
+const axios = require('axios')
+const router=express.Router()
+const moment = require('moment')
+
+
+
+router.get('/',async(req,res)=>{
+    try {
+        var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=in&' +
+          'apiKey=2637b1dde1f74f1d811fee2fe118b46b';
+
+        const news_get =await axios.get(url)
+        res.render('index',{articles:news_get.data.articles})
+
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
 })
 
-
-
-module.exports=router;
+module.exports=router
